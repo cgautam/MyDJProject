be6272e6f7aca9e943ddb3dd0c6bb8249ceae418
Gautam kumar <gautam.kumar@continental-corporation.com>
2017-03-24 16:18:30 +0530

    INFO : Some more experiment on adding records and displaying in django shell terminal.

--- MyProject/QuesAns/models.py
+++ MyProject/QuesAns/models.py
@@ -1,14 +1,30 @@
 from __future__ import unicode_literals
 
+
+import datetime
+
+
 from django.db import models
+from django.utils import timezone
+
 
 class Ques(models.Model):
     ques_text = models.CharField(max_length=200)
     q_date = models.DateTimeField('date assigned')
 
 
+    def __unicode__(self):
+        return self.ques_text
+
+    def published_now(self):
+        return self.q_date >= timezone.now() - datetime.timedelta(days=1)
+
+
 class Choice(models.Model):
 	ques = models.ForeignKey(Ques)
 	choice_text = models.CharField(max_length=200)
 	votes = models.IntegerField(default=0)
-# Create your models here.
+
+
+	def __unicode__(self):
+		return self.choice_text
